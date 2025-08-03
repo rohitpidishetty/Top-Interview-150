e2baa5a9c7cd2eed3d9071df2c6478383c16110b
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-08-03 13:09:15 -0500

    Add files via upload

---
+++ _209_Minimum_Size_Subarray_Sum.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Solution {
+
+  // Let us use sliding window
+  public int minSubArrayLen(int target, int[] nums) {
+    Queue<Integer> slidingWindow = new ArrayDeque<>();
+    int j = 0;
+    int minSize = Integer.MAX_VALUE;
+    int currentSum = 0;
+    while (j < nums.length) {
+      slidingWindow.add(nums[j]);
+      currentSum += nums[j];
+      while (!slidingWindow.isEmpty() && currentSum >= target) {
+        minSize = Math.min(minSize, slidingWindow.size());
+        int outOfBoundValue = slidingWindow.poll();
+        currentSum -= outOfBoundValue;
+      }
+      j++;
+    }
+    return minSize == Integer.MAX_VALUE ? 0 : minSize;
+  }
+}
+
+public class _209_Minimum_Size_Subarray_Sum {
+
+  public static void main(String[] args) {
+    System.out.println(
+      new Solution().minSubArrayLen(11, new int[] { 1, 2, 3, 4, 5 })
+    );
+  }
+}
