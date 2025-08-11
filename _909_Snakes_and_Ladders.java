7d3d26ed5f340ece85a46e7cc6b2fd805f6f2847
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2025-08-10 20:55:07 -0500

    Add files via upload

---
+++ _909_Snakes_and_Ladders.java
@@ -0,0 +1,116 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+class Solution {
+
+  private Map<Integer, int[]> getCoordinates(int[][] board, int n, int n2) {
+    int i = n - 1;
+    int j = 0;
+    boolean direction = true;
+    Map<Integer, int[]> coords = new HashMap<>();
+    int idx = 1;
+    while (n2 > 0) {
+      coords.put(idx++, new int[] { i, j });
+      if (direction) {
+        if (j == n - 1) {
+          i--;
+          direction = false;
+        } else j++;
+      } else {
+        if (j == 0) {
+          i--;
+          direction = true;
+        } else j--;
+      }
+      n2--;
+    }
+    return coords;
+  }
+
+  public int snakesAndLadders(int[][] board) {
+    int n = board.length;
+    int n2 = n * n;
+
+    Map<Integer, int[]> coords = getCoordinates(board, n, n2);
+
+    int ret = bfs(1, board, coords, n2);
+
+    return ret;
+  }
+
+  private int bfs(
+    int start,
+    int[][] board,
+    Map<Integer, int[]> coords,
+    int goalState
+  ) {
+    Set<Integer> visited = new HashSet<>();
+
+    Queue<Integer> bfsQ = new ArrayDeque<>();
+    bfsQ.offer(1);
+    visited.add(1);
+
+    int steps = 0;
+
+    while (!bfsQ.isEmpty()) {
+      System.out.println(bfsQ);
+      int size = bfsQ.size();
+      while (size > 0) {
+        // Get current coord and see all its reachables starting from (curr + 1 to curr + 6).
+        int currentCoord = bfsQ.poll();
+
+        if (currentCoord == goalState) return steps;
+
+        // Neighbors
+        for (
+          int roll = currentCoord + 1;
+          roll <= Math.min(goalState, currentCoord + 6);
+          roll++
+        ) {
+          int coord[] = coords.get(roll);
+          int v = board[coord[0]][coord[1]];
+          if (v == -1) {
+            if (!visited.contains(roll)) {
+              bfsQ.add(roll);
+              visited.add(roll);
+            }
+          } else {
+            if (!visited.contains(v)) {
+              bfsQ.offer(v);
+              visited.add(v);
+            }
+          }
+        }
+        size--;
+      }
+      steps++;
+    }
+    return -1;
+  }
+}
+
+public class _909_Snakes_and_Ladders {
+
+  public static void main(String[] args) {
+    System.out.println(
+      new Solution()
+        .snakesAndLadders(
+          new int[][] {
+            { -1, -1, -1, -1, -1, -1 },
+            { -1, -1, -1, -1, -1, -1 },
+            { -1, -1, -1, -1, -1, -1 },
+            { -1, 35, -1, -1, 13, -1 },
+            { -1, -1, -1, -1, -1, -1 },
+            { -1, 15, -1, -1, -1, -1 },
+          }
+        )
+    );
+  }
+}
